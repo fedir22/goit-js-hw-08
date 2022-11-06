6a673582fdd79ffb3c6971723a5aa60174067235
fedir22 <levchenkofedor@gmail.com>
2022-11-06 15:07:22 +0200

    hw-js-08-ff

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,4 @@
+
 import throttle from "lodash.throttle";
 
 const STORAGE_KEY = 'feedback-form-state';
@@ -10,16 +11,19 @@ let formData = {};
 savedFormData();
 
 refs.form.addEventListener('submit', onFormSubmit);
+
 refs.form.addEventListener('input', throttle(e => {
     formData[e.target.name] = e.target.value;
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-    console.log(formData);
+    // console.log(formData);
 }, 500));
 
 function onFormSubmit(evt) {
     evt.preventDefault();
     evt.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
+    console.log(formData);
+    formData = {};
 };
 function savedFormData() {
     const savedForm = localStorage.getItem(STORAGE_KEY);
@@ -28,6 +32,6 @@ function savedFormData() {
         formData = parsedFormData;
         refs.email.value = formData.email || '';
         refs.textarea.value = formData.message || '';
-        console.log(formData);
+        // console.log(formData);
     }
 };
